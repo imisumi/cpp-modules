41deb0f3758507c5298fe41bd29667af1b64519a
imisumi <ichiromisumi@outlook.com>
2024-02-08 03:51:53 +0100

    cpp01 ex4

--- cpp01/ex04/src/main.cpp
+++ cpp01/ex04/src/main.cpp
@@ -2,7 +2,18 @@
 #include <fstream>
 
 
+void	replace(std::string &line, const std::string &s1, const std::string &s2, std::size_t pos)
+{
+	std::size_t found = line.find(s1, pos);
+	if (found != std::string::npos)
+	{
+		line.insert(found + s1.size(), s2);
+		line.erase(found, s1.size());
+		replace(line, s1, s2, found + s2.size());
+	}
+}
 
+// intput  filename s1 s2
 int main(int argc, char *argv[])
 {
 	if (argc != 4)
@@ -23,8 +34,9 @@ int main(int argc, char *argv[])
 	std::string line;
 	while (std::getline(file, line))
 	{
+		replace(line, s1, s2, 0);
 		std::cout << line << std::endl;
 	}
-	
+
 	return EXIT_SUCCESS;
 }
