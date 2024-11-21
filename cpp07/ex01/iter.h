7de72b05a7a7551027c02fc9cc818da821d3110a
imisumi <ichiromisumi@outlook.com>
2024-11-21 13:44:02 +0100

    added const iter

--- cpp07/ex01/iter.h
+++ cpp07/ex01/iter.h
@@ -10,3 +10,13 @@ void iter(T* array, size_t length, Func func)
 		func(array[i]);
 	}
 }
+
+// const version
+template <typename T, typename Func>
+void iter(const T* array, size_t length, Func func)
+{
+	for (size_t i = 0; i < length; i++)
+	{
+		func(array[i]);
+	}
+}
